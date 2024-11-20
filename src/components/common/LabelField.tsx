eb4202a73a39809777ed930e9cf35ab0b878323d
raj-pitroda <raj.pitroda@tatvasoft.com>
2024-11-20 15:04:17 +0530

    Added code for label field

---
+++ src/components/common/LabelField.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { IGenericFieldProps } from "../../service/commonModel";
+import { useField } from "formik";
+
+const LabelField: FC<IGenericFieldProps> = (props) => {
+  console.log(
+    "------rajlog---props.variableStyle",
+    props.variableStyle,
+    props.lbl
+  );
+
+  const { t } = useTranslation();
+  const { name, lbl, variableStyle } = props;
+  const [field] = useField(name);
+
+  return (
+    <div className="fieldWrapper">
+      {!variableStyle?.hideLabel && <label>{t(lbl ? lbl : name)}</label>}
+      {variableStyle?.isHtml ? (
+        <div dangerouslySetInnerHTML={{ __html: field.value }}></div>
+      ) : (
+        <>{field.value}</>
+      )}
+    </div>
+  );
+};
+
+export default LabelField;
